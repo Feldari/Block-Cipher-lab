43c0521c5c51a1544a1f681ca02ae95cbfbb4ec0
Feldari <35117006+Feldari@users.noreply.github.com>
2018-02-06 22:35:44 -0700

    Add files via upload

--- block_cipher.py
+++ block_cipher.py
@@ -9,7 +9,7 @@ def key():
 
     # keyin = input('Enter your text key: ').upper()
     # swap comments between above and below for debugging and live
-    keyin = 'good times test string'.upper()
+    keyin = 'test string'.upper()
 
     # sort the proposed key alphabetically
     sortin = sorted(keyin)
@@ -30,42 +30,42 @@ def encode(key):
     Block of code to encode your message
     key should be a randomly ordered list of numbers which contain numbers from 
     0 to len(key)-1
+
+    import random required to make this work
     '''
     # original = input('enter text string to encode: ').upper()
     # swap comments on above and below line to finish or debug program
     original = 'big long test string to encode as test string'.upper()
 
-    nospace = ''
+    nospace = []
 
     # remove spaces and non-letters from encoding string
     for lett in original:
         if (ord(lett) < 65) | (ord(lett) > 90):
             pass
         else:
-            nospace += lett
+            nospace.append(lett)
 
     # add random letters to end of plaintext to 
     # make plaintext block match key size
     while (len(nospace) % len(key)) != 0:
-        nospace += chr(random.randint(65, 90))
+        nospace.append(chr(random.randint(65, 90)))
 
-    # create list as long as nospace to manipulate
-    encoded = []
-    for x in range(len(nospace)):
-        encoded.append('.')
-
+    ##################################################
+    # i have a list ready for encoding i just need to figure out how to
+    # break the list into the consituant blocks. maybe i can use list 
+    # slicing [0:len(nospace)] or some such nonsense
+    ##################################################
     final = ''
     for char in key:
         x = int(char)
         final = final + nospace[x]
-    print(final)
-    print(key)
+
+
     print(nospace)
-    '''
-    encoded.insert(key[nospace.index(char) % len(key)], char)
-    encoded.pop(key[nospace.index(char) % len(key)]+1)
-    print(encoded)
-    '''
+    print(key)
+    print(final)
+
 
 if __name__ == '__main__':
 
