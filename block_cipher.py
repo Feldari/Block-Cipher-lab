ab12ba2609dd606da11f8dd6b1c4ec023aa8e57b
Feldari <35117006+Feldari@users.noreply.github.com>
2018-02-08 09:01:49 -0700

    Add files via upload

--- block_cipher.py
+++ block_cipher.py
@@ -8,7 +8,7 @@ def key():
     keyout = []
     keyin = ''
 
-    userin = input('Enter your text key: ').upper()
+    userin = input('\nEnter your text key: ').upper()
     # swap comments between above and below for debugging and live
     # userin = 'test string'.upper()
 
@@ -38,24 +38,9 @@ def encode(key):
 
     import random required to make this work
     '''
-    original = input('enter text string to encode: ').upper()
+    original = input('\nEnter text string to encode: ').upper()
     # swap comments on above and below line to finish or debug program
     # original = 'big long test string to encode as test string'.upper()
-
-    '''
-    nospace = []
-    # remove spaces and non-letters from encoding string put string into list
-    for lett in original:
-        if (ord(lett) < 65) | (ord(lett) > 90):
-            pass
-        else:
-            nospace.append(lett)
-
-    # add random letters to end of plaintext to 
-    # make plaintext block match key size
-    while (len(nospace) % len(key)) != 0:
-        nospace.append(chr(random.randint(65, 90)))
-    '''
 
     nospace = '' 
     # remove spaces and non-letters from encoding string put string into list
@@ -88,25 +73,10 @@ def decode(key):
 
     import random required to make this work
     '''
-    original = input('enter text string to decode: ').upper()
+    original = input('\nEnter text string to decode: ').upper()
     # swap comments on above and below line to finish or debug program
     # original = 'big long test string to encode as test string'.upper()
     # original = encode(key)
-
-    '''
-    nospace = []
-    # remove spaces and non-letters from encoding string put string into list
-    for lett in original:
-        if (ord(lett) < 65) | (ord(lett) > 90):
-            pass
-        else:
-            nospace.append(lett)
-
-    # add random letters to end of plaintext to 
-    # make plaintext block match key size
-    while (len(nospace) % len(key)) != 0:
-        nospace.append(chr(random.randint(65, 90)))
-    '''
 
     nospace = '' 
     # remove spaces and non-letters from encoding string put string into list
@@ -134,6 +104,23 @@ if __name__ == '__main__':
 
     import random
 
-    print(encode(key()))
-    print(decode(key()))
+    exit = 0
+    while exit != 1:
+        print('What would you like to do?')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Exit')
+        option = input('>>').upper()
+        
+        if  option == 'ENCODE' or option == '1':
+            print('\nYour encoded string:\n', encode(key()), '\n\n', sep='')
+
+        elif option == 'DECODE' or option == '2':
+            print('\nYour decoded string:\n', decode(key()), '\n\n', sep='')
+
+        elif option == 'EXIT' or option == '3':
+            exit = 1
+
+        else:
+            print('Please enter a valid option!')
 
